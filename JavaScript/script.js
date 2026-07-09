0f12a5dbe8010dfad08ad27c714e62059195cf3f
Rohan Lokhande <rohanlokhande1306@gmail.com>
2026-07-09 22:54:33 +0530

    Describe what you changed

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,5 +1,15 @@
 const themeButton = document.querySelector(".theme");
-themeButton.addEventListener("click",function()
-{
-   document.body.classList.toggle("dark-mode");
+   const savedTheme = localStorage.getItem("theme");
+
+      if (savedTheme === "dark") {
+         document.body.classList.add("dark-mode");
+      }
+      themeButton.addEventListener("click", function () {
+         document.body.classList.toggle("dark-mode");
+         if (document.body.classList.contains("dark-mode")) {
+            localStorage.setItem("theme", "dark");
+         } else {
+            localStorage.setItem("theme", "light");
+         }
+
 });
